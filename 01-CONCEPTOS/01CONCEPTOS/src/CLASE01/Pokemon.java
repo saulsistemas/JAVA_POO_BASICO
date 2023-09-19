722d0ff416a2d1cfdde0d81db6778c6d56d2b991
saulsistemas <santamariaramos18@hotmail.com>
2023-09-18 22:55:55 -0500

    metodos, static

--- 01-CONCEPTOS/01CONCEPTOS/src/CLASE01/Pokemon.java
+++ 01-CONCEPTOS/01CONCEPTOS/src/CLASE01/Pokemon.java
@@ -2,5 +2,29 @@
 package CLASE01;
 //Clase - Es una plantilla que contienen la estructura básica de un objeto (Atributos y Métodos).
 public class Pokemon {
-
+    //Métodos - Acciones o funciones que pueden ser llamadas dentro de la clase o por otra clase.
+    //Parametros = lo que esta entre los parentesis
+    
+    //Static - permite acceder a las variables y métodos aunque no tengamos una instancia
+    //del objeto que los contiene
+    //Regla de Metodos estaticos, no pueden agregar metodos que no sean estaticos.
+    
+    public static void main(String[] args) {
+        
+        ataquePokemon("Trueno");
+        ataquePokemon("Latigo");
+        
+//        Pokemon pk = new Pokemon();
+//        pk.nombrePokemon();
+    }
+    
+    //Ejemplo de metodos
+    public void nombrePokemon(){
+        System.out.println("Hola soy un pokemon");
+        
+    }
+    
+    public static void ataquePokemon(String ataque){
+        System.out.println("Tengo los siguientes ataques "+ ataque);
+    }
 }

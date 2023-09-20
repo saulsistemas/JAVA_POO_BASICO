9f3f7957eb4b161f1792c021c759978f47652cbc
saulsistemas <santamariaramos18@hotmail.com>
2023-09-19 22:09:03 -0500

    Bucle For

---
+++ 01-CONCEPTOS/01CONCEPTOS/src/CLASE08/BucleFor.java
@@ -0,0 +1,22 @@
+
+package CLASE08;
+
+/**
+ *
+ * Bucle For -es una estructura iterativa para ejecutar un mismo 
+ * segmento de código una cantidad de veces desead
+ */
+public class BucleFor {
+    
+    public static void main(String[] args) {
+//        for(int i = 0; // índice de control
+//        i < 100;   // condición booleana
+//        i++)      // modificación del índice tras cada bucle
+    
+        int valor = 15;
+        for (int i = 0; i < valor; i++) {
+            System.out.println("Bucle For "+i);
+        }
+    }
+    
+}

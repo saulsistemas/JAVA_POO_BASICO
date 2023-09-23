c4044c1cd86de607244521ecfd38cd208005d537
saulsistemas <santamariaramos18@hotmail.com>
2023-09-23 10:25:19 -0500

    Habilitando transacciones Statement y ResultSet

--- 02-JDBC/JavaApplication3/src/connection/ConnectionFactory.java
+++ 02-JDBC/JavaApplication3/src/connection/ConnectionFactory.java
@@ -3,6 +3,8 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 /**
  *
  * @author HELPNET
@@ -11,6 +13,9 @@ public class ConnectionFactory {
     //objeto Connection(Tipo) Variable
     //Variable de tipo Connection
     private static Connection connection;
+    
+    //objeto encargado de habilitar y ejecutar las sentencias SQL
+    private static Statement statement;
     
     public static Connection conectar() throws ClassNotFoundException, SQLException{
         //1 - Cargar Driver
@@ -24,7 +29,23 @@ public class ConnectionFactory {
         //3 - Establecer la conexion por metodo
         connection = DriverManager.getConnection(url,user,password);
         
+        statement = connection.createStatement();//Habilitando proceso para ejecutar sentencia SQL
+        
         return connection;
         
+    }
+    
+    //Permite ejecutar Sentencias INSERT UPDATE y DELETE
+    //retorna 1 en caso de ser exitoso y 0 si es null
+    public static int ejecutarSQL(String sql) throws SQLException{
+        System.out.println("Query: "+sql);
+        return statement.executeUpdate(sql);
+    }
+    
+    //Permite ejecutar Sentencias SELECT
+    //retorna 1 objeto con informacion contenida de la sentencia SQL
+    public static ResultSet ejecutarSQLSelect(String sql) throws SQLException{
+        System.out.println("Query: "+sql);
+        return statement.executeQuery(sql);
     }
 }

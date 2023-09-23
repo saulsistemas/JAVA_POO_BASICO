201837cbf6f44c082c94c9747315c46cab3ebcfd
saulsistemas <santamariaramos18@hotmail.com>
2023-09-23 09:42:16 -0500

    Creando clases entity y herencia

--- 02-JDBC/JavaApplication3/src/entity/Restaurante.java
+++ 02-JDBC/JavaApplication3/src/entity/Restaurante.java
@@ -5,11 +5,60 @@ package entity;
  *
  * @author HELPNET
  */
-public class Restaurante {
+public class Restaurante extends CommonEntity{
     private int idRestaurante;
     private String nombre;
     private String imagen;
     private String slogan;
-    private TipoRestaurante idTipoRestaurante;
-    private Menu idMenu;
+    private TipoRestaurante tipoRestaurante;
+    private Menu menu;
+
+    public int getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(int idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public void setSlogan(String slogan) {
+        this.slogan = slogan;
+    }
+
+    public TipoRestaurante getTipoRestaurante() {
+        return tipoRestaurante;
+    }
+
+    public void setTipoRestaurante(TipoRestaurante tipoRestaurante) {
+        this.tipoRestaurante = tipoRestaurante;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+    
 }
